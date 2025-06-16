5d7c9fcb61032a9311c928b9eb2cdcefabc79962
Peter Ebert <ebertp@hhu.de>
2025-06-16 15:06:33 +0200

    extend check realign prec script with family color match

--- codebase/postproc-region-labels/workflow/scripts/check_realign_precision.py
+++ codebase/postproc-region-labels/workflow/scripts/check_realign_precision.py
@@ -102,6 +102,27 @@ def add_asm_seq_length(joined):
     return joined
 
 
+def check_same_color_family(region_label, matched_names):
+
+    possible_colors = ["green", "blue", "yellow", "red", "teal", "gray"]
+    region_color = None
+    for c in possible_colors:
+        if c not in region_label:
+            continue
+        assert region_color is None, "Multiple colors found in region label"
+        region_color = c
+    if region_color is None:
+        # that region is simply not a 'color region'
+        return []
+
+    family_match = []
+    for idx, name in matched_names.items():
+        if region_color not in name:
+            continue
+        family_match.append(idx)
+    return family_match
+
+
 def merge_aligned_regions(joined):
 
     out_regions = []
@@ -109,18 +130,38 @@ def merge_aligned_regions(joined):
         offset = alignments["asm_seq_offset"].iloc[0]
         if alignments["Name"].nunique() != 1:
             if alignments["asm_region_label"].iloc[0] not in alignments["Name"].values:
-                # uncertain - might be an alignment artifact
-                orientation = alignments.groupby("aln_strand")["Overlap"].sum()
-                orientation = orientation.index[orientation.argmax()]
-                region = {
-                    "seq": alignments["asm_seq"].iloc[0],
-                    "start": alignments["asm_seq_start"].min() + offset,
-                    "end": alignments["asm_seq_end"].max() + offset,
-                    "name": "uncertain", # label
-                    "score": 0,
-                    "strand": orientation,
-                    "asm_cutout": alignments["asm_seq_name"].iloc[0]
-                }
+                # two options: if it's a 'color family' match, it's likely just
+                # an alignment artifact (alignment-based matching is not sensitive enough)
+                # otherwise, no clue what the problem is / where the mismatch comes from
+                region_label_name = alignments["asm_region_label"].iloc[0]
+                matched_names = alignments["Name"]
+                matches_by_family = check_same_color_family(region_label_name, matched_names)
+                if matches_by_family:
+                    subset = alignments.loc[matches_by_family, :].copy()
+                    orientation = subset.groupby("aln_strand")["Overlap"].sum()
+                    orientation = orientation.index[orientation.argmax()]
+                    region = {
+                        "seq": subset["asm_seq"].iloc[0],
+                        "start": subset["asm_seq_start"].min() + offset,
+                        "end": subset["asm_seq_end"].max() + offset,
+                        "name": region_label_name, # label
+                        "score": 500,
+                        "strand": orientation,
+                        "asm_cutout": subset["asm_seq_name"].iloc[0]
+                    }
+                else:
+                    # uncertain - might be an alignment artifact
+                    orientation = alignments.groupby("aln_strand")["Overlap"].sum()
+                    orientation = orientation.index[orientation.argmax()]
+                    region = {
+                        "seq": alignments["asm_seq"].iloc[0],
+                        "start": alignments["asm_seq_start"].min() + offset,
+                        "end": alignments["asm_seq_end"].max() + offset,
+                        "name": "uncertain", # label
+                        "score": 0,
+                        "strand": orientation,
+                        "asm_cutout": alignments["asm_seq_name"].iloc[0]
+                    }
             else:
                 # only look at the subset with matching label;
                 # this is a heuristic that ignores alignment artifacts
@@ -133,7 +174,7 @@ def merge_aligned_regions(joined):
                     "start": subset["asm_seq_start"].min() + offset,
                     "end": subset["asm_seq_end"].max() + offset,
                     "name": subset["Name"].iloc[0], # label, # label
-                    "score": 500,
+                    "score": 750,
                     "strand": orientation,
                     "asm_cutout": subset["asm_seq_name"].iloc[0]
                 }
